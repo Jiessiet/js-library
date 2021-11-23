fef97fce90c7f301d71605f7aa86c2808bbf1a2b
tiechu <jiessie.tie@mail.utoronto.ca>
2021-11-22 19:17:19 -0500

    styling sizes

--- public/examples.js
+++ public/examples.js
@@ -3,18 +3,18 @@
 const cell0paragraph = new TableCellGenerator()
 cell0paragraph.makeHeaderCell('400px', '600px', '100px', '300px', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minus impedit maxime, quae soluta quis cumque perferendis! Doloribus quaerat, placeat iste facere, aspernatur ex cum veritatis laudantium, officia, non porro exercitationem incidunt quis dolore? Officia ex accusamus expedita optio, voluptatem minus? In maiores omnis aperiam earum ab molestiae beatae laborum blanditiis incidunt, delectus dolor, id voluptates optio aspernatur aliquam saepe atque labore? Tempore reprehenderit ab ipsam perspiciatis ut, provident perferendis sapiente in numquam blanditiis, enim, illo error nulla incidunt quos quidem ratione repellat ipsa molestias veritatis? Mollitia, fugit dolore commodi porro repudiandae atque, eos, ipsum quam culpa fuga deleniti quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minus impedit maxime, quae soluta quis cumque perferendis! Doloribus quaerat, placeat iste facere, aspernatur ex cum veritatis laudantium, officia, non porro exercitationem incidunt quis dolore? Officia ex accusamus expedita optio, voluptatem minus? In maiores omnis aperiam earum ab molestiae beatae laborum blanditiis incidunt, delectus dolor, id voluptates optio aspernatur aliquam saepe atque labore? Tempore reprehenderit ab ipsam perspiciatis ut, provident perferendis sapiente in numquam blanditiis, enim, illo error nulla incidunt quos quidem ')
 cell0paragraph.changeCellsColor('#6b705c', 0)
-cell0paragraph.makeHeaderCell('300px', '70px', '100px', '900px', "Click the true answer")
+cell0paragraph.makeHeaderCell('300px', '70px', '100px', '850px', "Click the true answer")
 cell0paragraph.changeCellsColor('#6b705c', 1)
 
 const cell1quiz = new TableCellGenerator()
 cell1quiz.makeQuizCells('240px', '120px', '150px', '750px','Quiz Option false', false)
 cell1quiz.makeQuizCells('240px', '120px', '350px', '750px','Quiz Option false', false)
-cell1quiz.makeQuizCells('240px', '120px', '150px', '1100px','Quiz Option true', true)
-cell1quiz.makeQuizCells('240px', '120px', '350px', '1100px','Quiz Option false', false)
+cell1quiz.makeQuizCells('240px', '120px', '150px', '1040px','Quiz Option true', true)
+cell1quiz.makeQuizCells('240px', '120px', '350px', '1040px','Quiz Option false', false)
 
 const cellForm = new TableCellGenerator()
 cellForm.makeHeaderCell('200px', '60px', '600px', '750px', 'What did you think?')
-cellForm.addUserInput('200px', '100px', '600px', '1070px')
+cellForm.addUserInput('200px', '100px', '600px', '1040px')
 cellForm.changeCellsColor('#6b705c', 0)
 
 const cellheader = new TableCellGenerator()
